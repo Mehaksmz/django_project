a9f1a714f3b40fb388a9f036f87d908154324502
Mehaksmz <125243766+Mehaksmz@users.noreply.github.com>
2025-07-04 14:19:08 +0400

    add Swagger UI documentation endpoint using drf-yasg

--- employee_project/employees/urls.py
+++ employee_project/employees/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EmployeeViewSet, DepartmentViewSet, PerformanceViewSet
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'employees', EmployeeViewSet)
 router.register(r'departments', DepartmentViewSet)
 router.register(r'performance', PerformanceViewSet)
 
+schema_view = get_schema_view(
+        openapi.Info(
+            title="Employee API",
+            default_version='v1',
+        ),
+        public=True,
+        permission_classes=(permissions.AllowAny,),
+    )
+
 urlpatterns = [
     path('', include(router.urls)),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
